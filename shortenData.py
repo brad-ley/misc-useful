6878ffb9d7ca5d8c9c93e5a40b93007b55fbf65a
Brad P <bdprice@ucsb.edu>
2020-08-01 22:41:43 -0700

    added a nice little progress bar

--- shortenData.py
+++ shortenData.py
@@ -7,29 +7,61 @@ sys.path.append('/Users/Brad/Documents/Research/code/python/misc-useful')
 from readDataFile import read
 
 
-def shrink(filename, start=0, end=-1):
+def shrink(filename, start=0, end=-1, directory='.'):
     header, data = read(filename)
     data = data[start:end, :]
-    savename = 'short_' + filename
+    savename = directory + '/short/' + filename
     np.savetxt(savename, data, header=header, delimiter=', ')
 
 
-def main(start=0, end=-1, directory='.'):
+def shorten(start=0, end=-1, directory='.'):
     filelist = [
         ii for ii in os.listdir(directory)
+        if ii.endswith('.dat')
+    ]
+    try:
+        os.makedirs(directory + '/short/')
+    except FileExistsError:
+        # directory already exists
+        pass
+
+    shortfilelist = [
+        ii for ii in os.listdir(directory + "/short/")
         if (ii.endswith('.dat'))
     ]
-    cur_file = filelist[0]
 
+    num = len([ii for ii in filelist if (ii.startswith('M') and ii not in shortfilelist)])
+    count = 0 
+    oldpercent = 0
+    percent = 0
+    ps = '|' + '-'*50 + '|'
+    print(f"{ps} {percent}% done", end='\r')
     for file in filelist:
-        if file.startswith('M') and 'short_' + file not in filelist:
-            nex_file = '_'.join(file.split('_')[:-1])
-
-            if nex_file != cur_file:
-                print(f"Finished {cur_file}")
-            cur_file = nex_file
-            shrink(file, start=start, end=end)
-
+        if file.startswith('M') and file not in shortfilelist:
+            shrink(file, start=start, end=end, directory=directory)
+            lines = open(directory + '/short/' + file, 'r').readlines()
+            for line in lines:
+                idx = lines.index(line)
+                if line.startswith('# '):
+                    line = line[2:]
+                    lines[idx] = line
+                else:
+                    data_idx = lines.index(line)
+                    blank_idx = data_idx - 1
+                    if lines[blank_idx] == '\n':
+                        lines[blank_idx] = ''
+                    break
+            newfile = open(directory + '/short/' + file, 'w')
+            for line in lines:
+                newfile.write(line)
+            newfile.close()
+            percent = count*100 // num
+            if percent > oldpercent:
+                ps = '|' + '='*(percent//2) + '-'*(50 - (percent//2)) + '|'
+                print(f"{ps} {percent}% done", end='\r')
+                oldpercent = percent
+            count += 1
+   
 
 if __name__ == "__main__":
-    main(start=10000)
+    shorten(start=10000, directory='./fold')
